505bca13c2e122cb71fbde7e09b09e0a272be09e
careverca <airmincgn@gmail.com>
2023-02-24 23:30:00 +0700

    fix put v3

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const { showFalsePath } = require('../controllers/index');
 
 router.get('/404', showFalsePath);
+router.patch('/404', showFalsePath);
 
 module.exports = router;
